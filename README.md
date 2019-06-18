5368c497048a06a48e5e21ef8b247d52ae957cd6
grimSkepsis <developer.mikehofer@gmail.com>
2019-06-18 08:43:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clarity-searchable-dropdown
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/clarity-searchable-dropdown)
---
+++ app/app.component.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, Observable } from 'rxjs/Rx';
+@Component({
+  selector: 'my-app',
+  styleUrls: ['app.component.css'],
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  public optionsArray = [];
+  public optionsCount = 60;
+  public filterString: string = "";
+  constructor() {
+
+  }
+
+  public ngOnInit(): void {
+    for(let x = 0; x < this.optionsCount; x++) {
+        this.optionsArray.push("Option " + x);
+    }
+    console.log(this.optionsArray);
+  }
+
+  public get filteredOptions(): Array<string> {
+    return this.optionsArray.filter(option => option.includes(this.filterString));
+  }
+
+}
